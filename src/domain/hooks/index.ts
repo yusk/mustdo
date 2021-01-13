99fcd02806ba5a77b52f9a2dfc5db276149c3c68
tsurumiii <tsurumi0302@gmail.com>
2021-01-13 14:52:59 +0900

    add TodoHooks & SampleHooks

---
+++ src/domain/hooks/index.ts
@@ -0,0 +1,9 @@
+import { SampleContext, SampleProvider } from "./sample/SampleHooks";
+import { useTodo } from "./todo/TodoHooks";
+
+
+export {
+  SampleContext,
+  SampleProvider,
+  useTodo,
+}
---
+++ src/domain/hooks/todo/TodoHooks.tsx
@@ -0,0 +1,57 @@
+import { useCallback, useState } from "react"
+import { useInjection } from "../../../di_container/InjectionProvider"
+import { Todo } from "../../entities/index"
+import { LocalDatabaseRepository } from "../../repository"
+
+export const useTodo = () => {
+  const { container } = useInjection()
+  // Repository
+  const localDatabaseRepository = container.resolve<LocalDatabaseRepository>('LocalDatabaseRepository');
+
+  // State
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>('')
+
+  // UseCase
+  const fetchTodo = useCallback(async (): Promise<Todo | undefined> => {
+    console.log("called fetchTodo")
+    if (loading) return undefined;
+    setLoading(true)
+    try {
+      // localStorageから取得
+      const result = await localDatabaseRepository.load<string>("currentTodo");
+      if (result == null) {
+        setLoading(false)
+        return
+      }
+      const todo = JSON.parse(result)
+      setLoading(false)
+      return todo
+    } catch (e) {
+      console.error('error', e)
+      setError(e.message)
+      setLoading(false)
+    }
+  }, [loading, localDatabaseRepository])
+
+  const saveCurrentTodo = useCallback(async (title: string): Promise<Todo> => {
+    console.log(`called saveTodo`)
+    const todo = new Todo(title, null)
+    await localDatabaseRepository.save<string>("currentTodo", JSON.stringify(todo))
+    return todo
+  }, [localDatabaseRepository])
+
+  const doneCurrentTodo = useCallback(async (currentTodo: Todo): Promise<Todo> => {
+    currentTodo.setDoneDate()
+    await localDatabaseRepository.remove("currentTodo")
+    return currentTodo
+  }, [localDatabaseRepository])
+
+  return {
+    fetchTodo,
+    saveCurrentTodo,
+    doneCurrentTodo,
+    loading,
+    error,
+  }
+}
